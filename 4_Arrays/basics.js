7952738434c98e43eddd6be1b4103471c712e194
Romain Candusso <r.candusso@protonmail.com>
2022-03-17 10:31:01 +0100

    Added the spread operator.

--- 4_Arrays/basics.js
+++ 4_Arrays/basics.js
@@ -17,7 +17,7 @@ hobbies = ['chess'];
 console.log(hobbies);
 
 /*
-Ne marche pas : réassigne une constante.
+Doesn't work : reassign a constant.
 chores = ['dishes'];
 console.log(chores);
 */
@@ -37,6 +37,14 @@ console.log(hobbies);
 
 console.log(hobbies.slice(1, 4));
 
-// Begins at index 1, replaces 3 elements by what comes after it.
+// Begins at index 1, replaces 3 elements and puts the other arguments where they were.
 hobbies.splice(1, 3, 'intellectual things');
 console.log(hobbies);
+
+// Copies a whole array into another
+let newHobbies = [...hobbies];
+newHobbies.push('coding');
+console.log('newHobbies array is now : ' + newHobbies.join(' '));
+let joeyHobbies = ['traveling', 'investing', 'taking photographs'];
+let ourHobbies = [...newHobbies, ...joeyHobbies];
+console.log('The array of hobbies for Romain and Joey are : ' + ourHobbies);
